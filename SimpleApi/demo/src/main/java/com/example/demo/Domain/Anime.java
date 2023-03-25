5c5b4bf172b8859cbd8ac022aaced8a0b895d560
JGMelon22 <jgvasconcellos22@gmail.com>
2023-03-25 15:53:06 -0300

    Spring Data JPA part 1

--- SimpleApi/demo/src/main/java/com/example/demo/Domain/Anime.java
+++ SimpleApi/demo/src/main/java/com/example/demo/Domain/Anime.java
@@ -1,11 +1,27 @@
 package com.example.demo.Domain;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data // Lombok feature - Getters/Setters/HashSet and Equals
 @AllArgsConstructor // Lombok feature - Constructors
+@NoArgsConstructor
+@Entity
+
+@Table(name = "animes")
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "name", columnDefinition = "varchar(100)")
     private String name;
 }
